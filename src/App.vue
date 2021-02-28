a75e05221b9a667796a5e6a6bbe1e6435b60536e
MashuMaru <52470960+MashuMaru@users.noreply.github.com>
2021-02-28 19:54:51 +0000

    imported css and components.

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <section>
+    <header>
+      <h1>Friends Contacts</h1>
+    </header>
+    <ul>
+      <friend-contact></friend-contact>
+      <friend-contact></friend-contact>
+    </ul>
+  </section>
+</template>
+
+<script>
+// import FriendContact from "./components/FriendContact.vue";
+export default {
+  // components: { FriendContact },
+  data: function() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Surname",
+          phone: "0123 443 2342",
+          email: "manuel@local.com",
+        },
+        {
+          id: "julie",
+          name: "Julie Surname",
+          phone: "0123 443 2342",
+          email: "julie@local.com",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Jost&display=swap');
+@import "../public/styles.css";
+</style>
---
+++ src/components/FriendContact.vue
@@ -0,0 +1,34 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button v-on:click="toggleDetails">{{detailsAreVisible ? 'Hide details' : 'Show details'}}</button>
+    <ul v-show="detailsAreVisible">
+      <li><strong>Phone:</strong>{{ friend.phone }}</li>
+      <li><strong>Email:</strong>{{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: 
+        {
+          id: "manuel",
+          name: "Manuel Surname",
+          phone: "0123 45678 90",
+          email: "manuel@email.com",
+        },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+};
+</script>
+
+<style></style>
